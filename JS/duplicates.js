ebb24d830fb0c5ab38ec07623e3a85e696d8b2a8
sukanya103 <138557311+sukanya103@users.noreply.github.com>
2024-07-04 10:26:12 +0530

    adding duplicate no code

---
+++ JS/duplicates.js
@@ -0,0 +1,23 @@
+var array='abccdnd'
+function duplicate(array, n){
+    var dup=[];
+for(var i=0; i<array.length; i++){
+    var d = false;
+    for(var j=i+1; j<array.length; j++){
+        if(array[j]==array[i])
+        {
+            d =true; 
+        }
+    }
+
+    if(d==n){
+
+        dup.push(array[i])
+    }
+    }
+
+    console.log(dup);
+}
+
+duplicate(array, true)
+duplicate(array, false)
