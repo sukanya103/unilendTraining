65e679f88996bb79aae1e074a41c2a1f4e056fea
sukanya103 <138557311+sukanya103@users.noreply.github.com>
2024-07-08 20:27:56 +0530

    Reset button feature

--- DOM/counter/test.js
+++ DOM/counter/test.js
@@ -8,17 +8,23 @@ const resetBtn = document.getElementById("reset-btn");
 
 let counter = 0;
 
-decrementBtn.addEventListener("click", () => {
+function decrement(){
   counter--;
   console.log("counter", counter);
   console.log("countValue", countValue);
   countValue.innerHTML = counter;
-});
+}
+decrementBtn.addEventListener("click",decrement);
 
 incrementBtn.addEventListener("click", () => {
   counter++;
   countValue.innerHTML = counter;
   console.log("counter", counter);
 });
+
+resetBtn.addEventListener('click',()=>{
+  counter=0
+  countValue.innerHTML=counter;
+})
 
 // Write method to reset the CountValue
