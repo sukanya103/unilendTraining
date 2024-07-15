6eb7655e0c487fce71c183a80f7ec02e2747f630
sukanya103 <138557311+sukanya103@users.noreply.github.com>
2024-07-15 22:45:09 +0530

    adding edit button

--- DOM/new_todo/todo.js
+++ DOM/new_todo/todo.js
@@ -1,6 +1,7 @@
 const input = document.querySelector("#input");
 const button = document.querySelector("#button");
 
+
 let arr = [];
 
 button.addEventListener("click", () => {
@@ -11,35 +12,48 @@ button.addEventListener("click", () => {
   };
 
   arr.push(obj);
-
+   input.value=" "
   print(arr);
 });
 
 function print(data) {
   const container = document.querySelector(".container");
   container.innerHTML = " ";
+  
   for (let i = 0; i < data.length; i++) {
     const newDiv = document.createElement("div");
     newDiv.setAttribute("class", "data");
     const p = document.createElement("p");
     const button1 = document.createElement("button");
     const button2 = document.createElement("button");
+    const button3 = document.createElement("button");
 
     p.innerHTML = data[i].text;
     button1.innerHTML = "Complete";
     if (data[i].complete == true) {
       button1.innerHTML = "completed";
     }
+
     button2.innerHTML = "delete";
+    button3.innerHTML= "Edit"
 
     newDiv.appendChild(p);
     newDiv.appendChild(button1);
     newDiv.appendChild(button2);
+    newDiv.appendChild(button3);
 
     container.appendChild(newDiv);
 
     button1.addEventListener("click", () => {
       completeTask(data[i].id); //pass function with parameters
+    });
+
+    button2.addEventListener("click", () => {
+        deleteTask(data[i].id); //pass function with parameters
+    });
+
+    button3.addEventListener("click", () => {
+        editTask(data[i].id); //pass function with parameters
     });
   }
 }
@@ -47,6 +61,7 @@ function print(data) {
 function completeTask(id) {
   console.log(id);
   const updatedarr = [];
+
   for (let i = 0; i < arr.length; i++) {
     if (id == arr[i].id) {
       const obj = {
@@ -62,3 +77,37 @@ function completeTask(id) {
   arr = updatedarr;
   print(updatedarr);
 }
+
+
+function deleteTask(id){
+    console.log(id)
+    let dele=[]
+    for(let i=0; i<arr.length; i++){ 
+    if(id != arr[i].id){
+        dele.push(arr[i]);
+    }
+    }
+    arr= dele;
+    print(dele)
+}
+
+function editTask(id) {
+    console.log(id);
+    const editarr = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (id == arr[i].id) {
+        const obj = {
+          id: arr[i].id,
+          text: input.value, //
+          complete: arr[i].complete,
+        };
+        editarr.push(obj);
+      } else {
+        editarr.push(arr[i]);
+      }
+    }
+    input.innerHTML=" "
+    arr = editarr;
+    print(editarr);
+  }
